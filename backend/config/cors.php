50ec5603a9ef162e167f608ddf565a1d6d8a03dd
Harindi <admin@Admins-MacBook-Pro.local>
2026-08-25 10:30:30 +0530

    Fix API CORS configuration

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,33 +2,23 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    */
-
     'paths' => [
         'api/*',
         'sanctum/csrf-cookie',
     ],
 
-    'allowed_methods' => [
-        '*',
-    ],
+    'allowed_methods' => ['*'],
 
-'allowed_origins' => [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'http://localhost:5183',
-    'http://127.0.0.1:5183',
-],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:5175',
+        'http://localhost:5176',
+    ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => [
-        '*',
-    ],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
